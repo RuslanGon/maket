8706f44178d0651b45c6409b7fe6e585894e936a
RuslanGon <ruslan441006@gmail.com>
2024-03-18 17:43:17 +0200

    ter

--- modal.js
+++ modal.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector('.btn-modal')
+const modal = document.querySelector('.modal')
+const backdrop = document.querySelector('.backdrop')
 
 btn.addEventListener('click', e => {
     if(e.target.nodeName !== 'BUTTON') return
     showModal()
 })
     
+backdrop.addEventListener('click', e => {
+    if(e.target === e.currentTarget)
+    hideModal()
+})
+
 
 function showModal (){
 document.body.classList.add('show-modal')
