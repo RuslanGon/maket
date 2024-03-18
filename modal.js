ea681f11195df418b6bbbcbb941f6003e9efd1dd
RuslanGon <ruslan441006@gmail.com>
2024-03-18 17:16:41 +0200

    rit

--- modal.js
+++ modal.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+const btn = document.querySelector('.btn-modal')
+
+btn.addEventListener('click', e => {
+    if(e.target.nodeName !== 'BUTTON') return
+    showModal()
+})
+    
+
+function showModal (){
+document.body.classList.add('show-modal')
+}
+
+function hideModal (){
+document.body.classList.remove('show-modal')
+}
+
